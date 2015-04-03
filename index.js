ffe8157c73c29f3db86d56255f3f1b50e845e467
Nicolas Montesdeoca <nico.racing.99@gmail.com>
2015-04-03 20:27:36 -0300

    exposing the module to be used form other project as a dependency

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-#!/usr/bin/env node
-"use strict";
-
-var subdivx = require('./subdivx');
-var args = process.argv.splice(2);
-
-var show = args[0];
-var releaseDetails = args.splice(1);
-
-console.log(['searching:', show, '-using details:', releaseDetails].join(' '));
-subdivx.downloadSubtitle(show, releaseDetails, './');
+module.exports = require('./subdivx');
+// #!/usr/bin/env node
+// "use strict";
+//
+// var subdivx = require('./subdivx');
+// var args = process.argv.splice(2);
+//
+// var show = args[0];
+// var releaseDetails = args.splice(1);
+//
+// console.log(['searching:', show, '-using details:', releaseDetails].join(' '));
+// subdivx.downloadSubtitle(show, releaseDetails, './');
